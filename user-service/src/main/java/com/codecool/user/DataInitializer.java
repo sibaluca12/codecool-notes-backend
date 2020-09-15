219d34ff45960cb73b8fab1f13926e096e183863
sibaluca12 <sibaluca@gmail.com>
2020-09-15 14:42:39 +0200

    datainitializer

--- user-service/src/main/java/com/codecool/user/DataInitializer.java
+++ user-service/src/main/java/com/codecool/user/DataInitializer.java
@@ -1,10 +1,13 @@
 package com.codecool.user;
 
+import com.codecool.user.entity.UserEntity;
 import com.codecool.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
 
 public class DataInitializer implements CommandLineRunner {
     @Autowired
@@ -19,6 +22,13 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        UserEntity user = UserEntity.builder()
+                .username("dinnye")
+                .password(passwordEncoder.encode("dinnye"))
+                .email("dinnye@dinnye.com")
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build();
 
+        userRepository.save(user);
     }
 }
